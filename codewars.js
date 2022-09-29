363422782524af94157acd4e8ba1e7dde1eba95f
Edward --TacoFed-- <61678819+dwrdmendoza@users.noreply.github.com>
2022-09-29 11:35:02 -0700

    Add files via upload

---
+++ codewars.js
@@ -0,0 +1,8 @@
+// We need a function that cna transform a string into a number. What ways of achieving this do you know.
+// example: "1234" --> 1234
+// "605" ---> 605
+
+// Solution
+const stringToNumber = function(str){
+  return Number(str);
+}
